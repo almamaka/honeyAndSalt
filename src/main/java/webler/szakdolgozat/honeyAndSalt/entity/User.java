5a0c473fbed7bb24d91cb045a75f48733805250f
almamaka <esztzi@gmail.com>
2020-12-10 18:58:44 +0100

    cleaning code from unused impports

--- src/main/java/webler/szakdolgozat/honeyAndSalt/entity/User.java
+++ src/main/java/webler/szakdolgozat/honeyAndSalt/entity/User.java
@@ -6,8 +6,6 @@ import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-
 @ToString(exclude = {"password", "likedRecipes"})
 @Entity
 @Table(name = "user")
--- src/main/java/webler/szakdolgozat/honeyAndSalt/service/SearchService.java
+++ src/main/java/webler/szakdolgozat/honeyAndSalt/service/SearchService.java
@@ -1,10 +1,7 @@
 package webler.szakdolgozat.honeyAndSalt.service;
 
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
